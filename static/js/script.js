7b7b3866d4b0bf37de095378fd4be507d8821891
subsonicplus <bahodirzurabaev@gmail.com>
2021-09-30 12:26:36 +0500

    removed needless events from js

--- static/js/script.js
+++ static/js/script.js
@@ -1,24 +1,22 @@
-window.onload = () => {
-	// portfolio demos
-	let works = document.querySelectorAll('.work button');
-	let lightbox = document.querySelector('#lightbox');	
+// portfolio demos
+let works = document.querySelectorAll('.work button');
+let lightbox = document.querySelector('#lightbox');	
 
-	works.forEach(elem => {
-		elem.onclick = () => window.open(elem.getAttribute('link'));
-	});
+works.forEach(elem => {
+	elem.onclick = () => window.open(elem.getAttribute('link'));
+});
 
-	// animation with the arrow for scrolling down
-	window.onscroll = () => {
-		let elem = document.querySelector('#intro a');
+// animation with the arrow for scrolling down
+window.onscroll = () => {
+	let elem = document.querySelector('#intro a');
 
-		if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-			elem.style.transform = 'rotate(180deg)';
+	if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+		elem.style.transform = 'rotate(180deg)';
 
-			elem.href = '#introduction';
-		} else {
-			elem.style.transform = 'rotate(0deg)';
+		elem.href = '#introduction';
+	} else {
+		elem.style.transform = 'rotate(0deg)';
 
-			elem.href = '#about';
-		}
+		elem.href = '#about';
 	}
 }
